b09d6acf609f5a44bbf25c297e0df4b25f8e5a5c
Yago Calomino <yagoferrocalomino@gmail.com>
2023-07-06 15:45:20 -0300

    req3-group-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Home /> } />
-    </Routes>
+    <>
+      <nav>
+        <Link
+          to="/shoppingcart"
+          data-testid="shopping-cart-button"
+        >
+          Carrinho de compras
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route
+          path="/shoppingcart"
+          element={ <ShoppingCart /> }
+        />
+      </Routes>
+    </>
   );
 }
 

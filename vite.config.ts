dcfe6e9f45f1693947cd65d036d2d8828d923d2f
Frederico Brion <fredericoasbrion@gmail.com>
2024-01-23 18:22:36 +0000

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend-store",
   plugins: [react()],
   test: {
     include: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
